f4e8d6d192700afe3d3b561280a5050e75511622
dkam26 <gkam1989@gmail.com>
2019-03-11 23:20:57 +0300

    Add flights model

--- flight_app/flights/models.py
+++ flight_app/flights/models.py
@@ -1,16 +1,42 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
 from django.conf import settings
+from django.contrib.postgres.fields import JSONField
 
 
-# Create your models here.
-class Flight(models.Model):
+
+class AvailableFlights(models.Model):
+    airline = models.CharField(max_length=255, blank=True, null=True)
+    origin = models.CharField(max_length=255, blank=True, null=True)
+    destination = models.CharField(max_length=50, blank=True, null=True)
+    available_seats = models.IntegerField(blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    plane_number = models.CharField(max_length=50, blank=True, null=True)
+    seats = JSONField()
+
+    class Meta:
+        managed = False
+        db_table = 'available_flights'
+
+
+class Tickets(models.Model):
+    airline = models.CharField(max_length=50, blank=True, null=True)
+    origin = models.CharField(max_length=50, blank=True, null=True)
+    destination = models.CharField(max_length=50, blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    plane_number = models.CharField(max_length=50, blank=True, null=True)
+    seat_number = models.CharField(max_length=50, blank=True, null=True)
     user = models.ForeignKey('user.User',
     related_name='flights',
     on_delete=models.CASCADE)
-    origin = models.CharField(max_length=255, blank=False)
-    destination = models.CharField(max_length=255, blank=False)
-    flight_date =  models.DateTimeField()
 
     def __str__(self):
-        """Return a human readable representation of the model instance."""
-        return "{}".format(self.origin)
+        return self.plane_number
+
--- flight_app/flights/views.py
+++ flight_app/flights/views.py
@@ -1,12 +1,29 @@
 from rest_framework import generics
-from .models import Flight
-from .serializers import FlightSerializer
+from .models import Tickets, AvailableFlights
+from .serializers import  ListFlightSerializer, FlightSerializer
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 from flight_app.settings import SECRET_KEY
 from rest_framework.authtoken.models import Token
 from django.core import serializers
+
+class ListFlightView(APIView):
+    serializer_class = ListFlightSerializer
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request, format=None):
+        # available_flights =  AvailableFlights.objects.filter(available_seats__gt=0)
+        # serializer = ListFlightSerializer(available_flights, many=True)
+        if request.META['HTTP_AUTHORIZATION']:
+            token = request.META['HTTP_AUTHORIZATION']
+            user_id = Token.objects.values_list('user_id', flat=True).get(key=token)
+            origin = request.data['origin']
+            destination = request.data['destination']
+            date = request.data['date']
+            flight_exists = AvailableFlights.objects.filter(origin=origin,destination=destination,date__contains = date, available_seats__gt=0)
+            serializer = ListFlightSerializer(flight_exists, many=True)
+            return Response(serializer.data)
+        return Response('No token provided')
 class CreateView(APIView):
     serializer_class = FlightSerializer
     permission_classes = (permissions.AllowAny,)
@@ -14,15 +31,30 @@ class CreateView(APIView):
         if request.META['HTTP_AUTHORIZATION']:
             token = request.META['HTTP_AUTHORIZATION']
             user_id = Token.objects.values_list('user_id', flat=True).get(key=token)
-            flight_data = request.data
-            flight_data['user'] = user_id
-            flight = Flight.objects.filter(origin=flight_data['origin'],destination=flight_data['destination'],flight_date=flight_data['flight_date'],user_id=flight_data['user']).exists()
-            if not flight:
-                serializer = FlightSerializer(data=flight_data)
-                if serializer.is_valid(raise_exception=True):
-                    serializer.save()
-                    return Response(serializer.data)
-            return Response('Flight exists')
+            origin = request.data['origin']
+            destination = request.data['destination']
+            date = request.data['date']
+            seat = request.data['seat']
+            airline = request.data['airline']
+            flight_exists = AvailableFlights.objects.filter(origin=origin,destination=destination,date__contains = date, available_seats__gt=0, airline=airline).first()
+            if flight_exists:
+                if seat in flight_exists.seats['seats']:
+                    serializer = FlightSerializer(data={'origin':flight_exists.origin,
+                                                        'destination':flight_exists.destination,
+                                                        'date':flight_exists.date,
+                                                        'airline':flight_exists.airline,
+                                                        'plane_number':flight_exists.plane_number,
+                                                        'seat_number':seat,
+                                                        'user':user_id})
+                    if serializer.is_valid():
+                        serializer.save()
+                        flight_exists.seats['seats'].pop(flight_exists.seats['seats'].index(seat))
+                        flight_exists.available_seats = flight_exists.available_seats - 1
+                        flight_exists.save()
+                        return Response(serializer.data)
+                else:
+                    return Response('Seat doesnt exist')
+            return Response('Flight doesnt exist')
         return Response('No token provided')
 
 
@@ -33,11 +65,10 @@ class DetailsView(APIView):
     serializer_class = FlightSerializer
 
     def get(self, request, format=None):
-        flight_data = {}
-        index = 0
         if request.META['HTTP_AUTHORIZATION']:
             token = request.META['HTTP_AUTHORIZATION']
             user_id = Token.objects.values_list('user_id', flat=True).get(key=token)
-            flights = Flight.objects.filter(user_id=user_id).all()
-            return Response(serializers.serialize('json', flights))
+            flights = Tickets.objects.filter(user_id=user_id).all()
+            serializer = FlightSerializer(flights, many=True)
+            return Response(serializer.data)
         return Response('No token provided')
