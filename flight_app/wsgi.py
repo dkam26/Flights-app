983de94b95f736309518bb1b456fff824a970705
dkam26 <gkam1989@gmail.com>
2019-04-01 21:53:05 +0300

    Host to heroku

--- flight_app/wsgi.py
+++ flight_app/wsgi.py
@@ -2,5 +2,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flight_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flight_app.flight_app.settings")
 application = get_wsgi_application()
