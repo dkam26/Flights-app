b4e4b570cbaec9e694c95f7a68d966385abf8048
dkam26 <gkam1989@gmail.com>
2019-04-01 21:00:58 +0300

    Host to heroku

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn flight_app.wsgi --log-file –
--- flight_app/flight_app/settings.py
+++ flight_app/flight_app/settings.py
@@ -14,6 +14,7 @@ import os
 import django_heroku
 
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -106,7 +107,6 @@ AUTH_USER_MODEL = 'user.User'
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -193,3 +193,5 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
