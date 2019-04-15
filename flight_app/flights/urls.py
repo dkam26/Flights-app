10f48f1c1a3078ad1d3a19c5eb0a2aa5e4e05889
dkam26 <gkam1989@gmail.com>
2019-04-15 21:45:31 +0300

    add redis worker

--- flight_app/flights/urls.py
+++ flight_app/flights/urls.py
@@ -4,7 +4,7 @@ from flight_app.flights.views import ListFlightView, CreateView, DetailsView, We
 
 
 urlpatterns = {
-    url(r'welcome/$',WelcomeView.as_view(), name='Welcome'),
+    url(r'/$',WelcomeView.as_view(), name='Welcome'),
     url(r'flights/$',ListFlightView.as_view(), name='flights'),
     url(r'book/flight/$',CreateView.as_view(), name='book flight'),
     url(r'user/flights/$',DetailsView.as_view(), name= 'user flights')
