b5f67f5a924ca1a53ac1ea3d9140a4010bc9e46e
dkam26 <gkam1989@gmail.com>
2019-04-15 22:36:07 +0300

    add redis worker

--- flight_app/flight_app/mailer.py
+++ flight_app/flight_app/mailer.py
@@ -6,4 +6,4 @@ def send_notification_email(name,email,date,origin,destination):
     event_date = date.strftime("%Y-%m-%d %H:%M")
     travel_date = event_date.split(" ")[0]
     travel_time = event_date.split(" ")[1]
-    send_mail(name,"Hey "+name+" your scheduled flight from "+origin+" to "+destination+" on the date "+travel_date+" and time "+travel_time+" is tomorrow!",'deo.kamara@andela.com',recipient_list,fail_silently=False)
+    send_mail(name,"Hey "+name+" your scheduled flight from "+origin+" to "+destination+" on the date "+travel_date+" and time "+travel_time+" !",'deo.kamara@andela.com',recipient_list,fail_silently=False)
